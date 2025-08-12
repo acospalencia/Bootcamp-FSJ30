3fdac436b07b1506391af83df7cccd4882afa408
AcostaPalencia <carlosacostacecsa@gmail.com>
2025-08-11 20:30:00 -0600

    clase 11/08

---
+++ REACT/concepts-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Saludo from './components/Saludo'
+
+function App() {
+  return (
+    <>
+      <h1>Concepts React</h1>
+
+      <Saludo nombre="Juan" />
+    </>
+  )
+}
+
+export default App
+
---
+++ REACT/concepts-project/src/components/Saludo.jsx
@@ -0,0 +1,10 @@
+
+function Saludo({nombre}) {
+  return (
+    <>
+      <h1>Hola, {nombre}!</h1>
+    </>
+  );
+}
+
+export default Saludo;
